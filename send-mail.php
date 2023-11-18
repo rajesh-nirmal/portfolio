a4720f6fb1934e3364eb6bede7d45da10ed1cec4
rajesh-nirmal <151131045+rajesh-nirmal@users.noreply.github.com>
2023-11-18 15:31:43 +0530

    Create send-mail.php
    Signed-off-by: rajesh-nirmal <151131045+rajesh-nirmal@users.noreply.github.com>

---
+++ send-mail.php
@@ -0,0 +1,30 @@
+<?php
+if(isset($_POST['submit']))
+{
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $number = $_POST['subject'];
+    $comment = $_POST['message'];
+    // $message = $_POST['message'];
+    // $date = $_POST['date'];
+    // $time = $_POST['time'];
+    
+    $to = "madhukarshroti2108@gmail.com";
+    
+    $subject = "New Message from - Name: $name, Subject: $number ";
+    
+    $messagetxt = "Name: $name\nEmail: $email\nSubject: $number\nMessage: $comment";
+    $headers  .= 'MIME-Version: 1.0' . "\r\n";
+    
+  if(mail($to,$subject,$messagetxt,$headers))
+  {
+      // echo "mail sent successfully";
+     header("Location:thank.html");
+      
+  }
+  else
+  {
+      echo "mail not sent";
+  }
+}
+?>
